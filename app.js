d132f518af0b58c197185c453c846873c6e0ce5f
MuslimBis <Lichking377@gmail.com>
2024-12-11 21:25:40 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+//1
+
+const number = 2;
+if (number < 10) {
+    console.log('константа меньше 10');
+}
+else{
+    console.log('константа не меньше 10');
+}
+
+//2
+
+console.log('start');
+
+if(3>4){
+    console.log('middle');
+} else if(3>2) {
+    console.log('success');
+} else console.log('error')
+
+//3
+
+let x = 10, y = 7;
+if(x > y){
+    console.log('x больше, чем y')
+} else{ 
+    console.log('x меньше, чем y')
+}
+
+//4
+
+let num = prompt('число');
+if(num % 2 === 0){
+    console.log('четное число')
+} else(console.log('нечетное число'))
+
+//5
+
+let num1 = prompt('1 числo');
+let num2 = prompt('2 числo');
+let num3 = prompt('3 числo');
+
+if(num1 >= num2, num3){
+    console.log(num1)
+} else if(num2 >= num1, num3){
+    console.log(num2)
+} else if(num3 >= num1, num3){
+    console.log(num3)
+} 
+
+
